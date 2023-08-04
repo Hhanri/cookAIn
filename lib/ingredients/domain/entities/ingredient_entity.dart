ffac77f2a7e4f2b5f3a5e33fbb9f028768e5eda2
HUYNH Henry <henry018@laposte.net>
2023-08-04 14:36:39 +0200

    Failure with code as String instead of int, small adjustment for ingredients_repository_interface, unit parser

--- lib/ingredients/domain/entities/ingredient_entity.dart
+++ lib/ingredients/domain/entities/ingredient_entity.dart
@@ -11,8 +11,24 @@ abstract class IngredientEntity extends Equatable {
     required this.unit
   });
 
+  Map<String, dynamic> toJson();
 }
 
 enum Unit {
   g, kg, mL, L
 }
+
+extension UnitExtension on String? {
+  Unit? parseUnit() {
+    switch(this) {
+      case 'g': return Unit.g;
+      case 'kg': return Unit.kg;
+      case 'mL': return Unit.mL;
+      case 'L': return Unit.L;
+      case null: return null;
+      default: return null;
+    }
+  }
+}
+
+typedef Ingredients = Map<String, IngredientEntity>;
--- lib/ingredients/domain/repository/ingredients_repository_interface.dart
+++ lib/ingredients/domain/repository/ingredients_repository_interface.dart
@@ -3,18 +3,18 @@ import 'package:cookain/core/failure/failure.dart';
 import 'package:cookain/ingredients/domain/entities/ingredient_entity.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class IngredientsRepositoryInterface {
+abstract class IngredientsRepositoryInterface<T extends IngredientEntity> {
 
-  Future<Either<Failure, void>> addIngredient({required IngredientEntity ingredient});
+  Future<Either<Failure, void>> addIngredient({required T ingredient});
 
   Future<Either<Failure, void>> removeIngredient({required String ingredientName});
 
-  Future<Either<Failure, void>> editIngredient({required IngredientEntity ingredient});
+  Future<Either<Failure, void>> editIngredient({required T ingredient});
 
   Future<Either<Failure, void>> removeQuantity({required String ingredientName, required num quantityToRemove});
 
   Future<Either<Failure, void>> addQuantity({required String ingredientName, required num quantityToAdd});
 
-  Stream<DocumentSnapshot<List<IngredientEntity>>> ingredientsStream();
+  Stream<DocumentSnapshot<Ingredients>> ingredientsStream();
 
 }
