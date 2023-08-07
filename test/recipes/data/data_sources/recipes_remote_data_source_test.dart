3dd509237cb1f6340605331cc0427c5dd9b4aaca
HUYNH Henry <henry018@laposte.net>
2023-08-07 20:37:14 +0200

    removed mock_recipe

--- test/recipes/data/data_sources/recipes_remote_data_source_test.dart
+++ test/recipes/data/data_sources/recipes_remote_data_source_test.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cookain/ingredients/data/data_sources/ingredients_remote_data_source.dart';
+import 'package:cookain/ingredients/data/models/ingredient_model.dart';
+import 'package:cookain/ingredients/domain/entities/ingredient_entity.dart';
 import 'package:cookain/recipes/data/data_sources/recipe_remote_data_source.dart';
+import 'package:cookain/recipes/data/models/recipe_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../mock/mock_firebase.dart';
-import '../../../mock/mock_recipe.dart';
-
 
 void main() {
 
@@ -18,29 +19,29 @@ void main() {
   group('recipes remote data source test', () {
 
     test('add ingredients', () async {
-      await ingredientsDataSource.addIngredient(ingredient: mockApple);
-      expect(fsi.dump(), mockAfterAddingApple);
-      await ingredientsDataSource.addIngredient(ingredient: mockSugar);
-      expect(fsi.dump(), mockAfterAddingSugar);
+      await ingredientsDataSource.addIngredient(ingredient: _mockApple);
+      expect(fsi.dump(), _mockAfterAddingApple);
+      await ingredientsDataSource.addIngredient(ingredient: _mockSugar);
+      expect(fsi.dump(), _mockAfterAddingSugar);
     });
 
     test('add recipe', () async {
-      await dataSource.addRecipe(mockRecipe);
-      expect(fsi.dump(), mockAfterAddingRecipe);
+      await dataSource.addRecipe(_mockRecipe);
+      expect(fsi.dump(), _mockAfterAddingRecipe);
     });
 
     test('edit recipe', () async {
-      await dataSource.editRecipe(mockEditedRecipe);
+      await dataSource.editRecipe(_mockEditedRecipe);
       expect(fsi.dump(), mockAfterEditingRecipe);
     });
 
     test('make recipe', () async {
-      await dataSource.makeRecipe(mockEditedRecipe);
+      await dataSource.makeRecipe(_mockEditedRecipe);
       expect(fsi.dump(), mockAfterMakingRecipe);
     });
 
     test('removing recipe', () async {
-      await dataSource.removeRecipe(mockRecipe.name);
+      await dataSource.removeRecipe(_mockRecipe.name);
       expect(fsi.dump(), mockAfterRemovingRecipe);
     });
 
@@ -52,3 +53,171 @@ void main() {
   });
 
 }
+
+const RecipeModel _mockRecipe = RecipeModel(
+  name: 'apple pie',
+  ingredients: {
+    'apple': IngredientModel(name: 'apple', quantity: 4, unit: null),
+    'sugar': IngredientModel(name: 'sugar', quantity: 50, unit: Unit.g),
+  }
+);
+
+const RecipeModel _mockEditedRecipe = RecipeModel(
+  name: 'apple pie',
+  ingredients: {
+    'apple': IngredientModel(name: 'apple', quantity: 2, unit: null),
+    'sugar': IngredientModel(name: 'sugar', quantity: 25, unit: Unit.g),
+  }
+);
+
+const _mockApple = IngredientModel(name: 'apple', quantity: 4, unit: null);
+const _mockSugar = IngredientModel(name: 'sugar', quantity: 50, unit: Unit.g);
+
+const _mockAfterAddingApple = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "apple": {
+        "name": "apple",
+        "quantity": 4,
+        "unit": null
+      }
+    }
+  }
+}''';
+
+const _mockAfterAddingSugar = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "apple": {
+        "name": "apple",
+        "quantity": 4,
+        "unit": null
+      },
+      "sugar": {
+        "name": "sugar",
+        "quantity": 50,
+        "unit": "g"
+      }
+    }
+  }
+}''';
+
+const _mockAfterAddingRecipe = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "recipes": {
+        "apple pie": {
+          "name": "apple pie",
+          "ingredients": {
+            "apple": {
+              "name": "apple",
+              "quantity": 4,
+              "unit": null
+            },
+            "sugar": {
+              "name": "sugar",
+              "quantity": 50,
+              "unit": "g"
+            }
+          }
+        }
+      },
+      "apple": {
+        "name": "apple",
+        "quantity": 4,
+        "unit": null
+      },
+      "sugar": {
+        "name": "sugar",
+        "quantity": 50,
+        "unit": "g"
+      }
+    }
+  }
+}''';
+
+const mockAfterEditingRecipe = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "recipes": {
+        "apple pie": {
+          "name": "apple pie",
+          "ingredients": {
+            "apple": {
+              "name": "apple",
+              "quantity": 2,
+              "unit": null
+            },
+            "sugar": {
+              "name": "sugar",
+              "quantity": 25,
+              "unit": "g"
+            }
+          }
+        }
+      },
+      "apple": {
+        "name": "apple",
+        "quantity": 4,
+        "unit": null
+      },
+      "sugar": {
+        "name": "sugar",
+        "quantity": 50,
+        "unit": "g"
+      }
+    }
+  }
+}''';
+
+const mockAfterMakingRecipe = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "recipes": {
+        "apple pie": {
+          "name": "apple pie",
+          "ingredients": {
+            "apple": {
+              "name": "apple",
+              "quantity": 2,
+              "unit": null
+            },
+            "sugar": {
+              "name": "sugar",
+              "quantity": 25,
+              "unit": "g"
+            }
+          }
+        }
+      },
+      "apple": {
+        "name": "apple",
+        "quantity": 2,
+        "unit": null
+      },
+      "sugar": {
+        "name": "sugar",
+        "quantity": 25,
+        "unit": "g"
+      }
+    }
+  }
+}''';
+
+const mockAfterRemovingRecipe = '''{
+  "ingredients": {
+    "uuid123456789": {
+      "recipes": {},
+      "apple": {
+        "name": "apple",
+        "quantity": 2,
+        "unit": null
+      },
+      "sugar": {
+        "name": "sugar",
+        "quantity": 25,
+        "unit": "g"
+      }
+    }
+  }
+}''';
