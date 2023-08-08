a0cc118781c65243f97f6f07cc68aba357f4bcaa
HUYNH Henry <henry018@laposte.net>
2023-08-08 17:45:23 +0200

    IngredientsCubit

---
+++ lib/ingredients/presentation/cubits/ingredients_cubit/ingredients_cubit.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cookain/core/cubits/my_bloc_state.dart';
+import 'package:cookain/ingredients/domain/entities/ingredient_entity.dart';
+import 'package:cookain/ingredients/domain/use_cases/add_ingredient_quantity_use_case.dart';
+import 'package:cookain/ingredients/domain/use_cases/add_ingredient_use_case.dart';
+import 'package:cookain/ingredients/domain/use_cases/edit_ingredient_use_case.dart';
+import 'package:cookain/ingredients/domain/use_cases/ingredients_stream_use_case.dart';
+import 'package:cookain/ingredients/domain/use_cases/remove_ingredient_quantity_use_case.dart';
+import 'package:cookain/ingredients/domain/use_cases/remove_ingredient_use_case.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'ingredients_state.dart';
+
+class IngredientsCubit extends Cubit<IngredientsState> {
+
+  final IngredientsStreamUseCase ingredientsStreamUseCase;
+  final AddIngredientUseCase addIngredientUseCase;
+  final EditIngredientUseCase editIngredientUseCase;
+  final RemoveIngredientUseCase removeIngredientUseCase;
+  final AddIngredientQuantityUseCase addIngredientQuantityUseCase;
+  final RemoveIngredientQuantityUseCase removeIngredientQuantityUseCase;
+
+  StreamSubscription<DocumentSnapshot<Ingredients>>? ingredientsStream;
+
+  IngredientsCubit({
+    required this.ingredientsStreamUseCase,
+    required this.addIngredientUseCase,
+    required this.editIngredientUseCase,
+    required this.removeIngredientUseCase,
+    required this.addIngredientQuantityUseCase,
+    required this.removeIngredientQuantityUseCase,
+  }) : super(const IngredientsInitial());
+
+  void init() {
+    ingredientsStream?.cancel();
+    ingredientsStream = ingredientsStreamUseCase.call().listen((event) {
+      emit(IngredientsSuccess(ingredients: event.data() ?? const {}));
+    });
+  }
+
+  @override
+  Future<void> close() async {
+    ingredientsStream?.cancel();
+    return super.close();
+  }
+
+}
---
+++ lib/ingredients/presentation/cubits/ingredients_cubit/ingredients_state.dart
@@ -0,0 +1,38 @@
+part of 'ingredients_cubit.dart';
+
+@immutable
+abstract class IngredientsState extends MyBlocState {
+  final Ingredients ingredients;
+  const IngredientsState({
+    required super.isLoading,
+    required this.ingredients,
+    super.error,
+    super.success,
+  });
+}
+
+class IngredientsInitial extends IngredientsState {
+  const IngredientsInitial() : super(isLoading: true, ingredients: const {});
+
+  @override
+  List<Object?> get props => [isLoading, ingredients, success, error];
+}
+
+class IngredientsSuccess extends IngredientsState {
+  const IngredientsSuccess({
+    required super.ingredients
+  }) : super(isLoading: false);
+
+  @override
+  List<Object?> get props => [isLoading];
+}
+
+class IngredientsError extends IngredientsState {
+  const IngredientsError({
+    required super.ingredients,
+    required super.error
+  }) : super(isLoading: false);
+
+  @override
+  List<Object?> get props => [isLoading, error];
+}
