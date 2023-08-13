f5ead4922686b4110fd8569be18dc375783dd0b4
HUYNH Henry <henry018@laposte.net>
2023-08-13 14:26:12 +0200

    fixed home navigation (removed default tab controller and added controller to cubit)

--- lib/home_navigation/presentation/cubits/home_navigation_cubit/home_navigation_cubit.dart
+++ lib/home_navigation/presentation/cubits/home_navigation_cubit/home_navigation_cubit.dart
@@ -1,10 +1,21 @@
 import 'package:cookain/home_navigation/domain/entities/home_nav_bar_item_entity.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeNavigationCubit extends Cubit<HomeNavigationState> {
+
+  final PageController controller = PageController();
+
   HomeNavigationCubit() : super(HomeNavigationState.ingredients);
 
   void changeView(HomeNavigationState state) {
     emit(state);
+    controller.jumpToPage(state.index);
+  }
+
+  @override
+  Future<void> close() async {
+    controller.dispose();
+    return super.close();
   }
 }
--- lib/home_navigation/presentation/pages/home_page.dart
+++ lib/home_navigation/presentation/pages/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:cookain/auth/presentation/cubits/auth_cubit.dart';
 import 'package:cookain/core/service_locator.dart';
 import 'package:cookain/home_navigation/domain/entities/home_nav_bar_item_entity.dart';
+import 'package:cookain/home_navigation/presentation/cubits/home_navigation_cubit/home_navigation_cubit.dart';
 import 'package:cookain/home_navigation/presentation/widgets/home_nav_bar.dart';
 import 'package:cookain/ingredients/presentation/cubits/ingredients_cubit/ingredients_cubit.dart';
 import 'package:cookain/ingredients/presentation/screens/ingredients_screen.dart';
 import 'package:cookain/ingredients/presentation/widgets/ingredient_dialog.dart';
+import 'package:cookain/recipes/presentation/cubits/recipes_cubit/recipes_cubit.dart';
+import 'package:cookain/recipes/presentation/screens/recipes_screen.dart';
+import 'package:cookain/recipes/presentation/widgets/recipe_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,27 +20,31 @@ class HomePage extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<IngredientsCubit>(create: (context) => sl.get<IngredientsCubit>()..init()),
+        BlocProvider<RecipesCubit>(create: (context) => sl.get<RecipesCubit>()..fetchMore()),
       ],
-      child: DefaultTabController(
-        length: HomeNavBarItemEntity.values.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("CookAIn"),
-            actions: [IconButton(onPressed: context.read<AuthCubit>().signOut, icon: const Icon(Icons.logout))],
-          ),
-          body: PageView(
-            physics: const NeverScrollableScrollPhysics(),
-            children: const [
-              IngredientsScreen(),
-              IngredientsScreen()
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => showAddIngredientDialog(context),
-            child: const Icon(Icons.add),
-          ),
-          bottomNavigationBar: const HomeNavBar(),
-        )
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("CookAIn"),
+          actions: [IconButton(onPressed: context.read<AuthCubit>().signOut, icon: const Icon(Icons.logout))],
+        ),
+        body: PageView(
+          controller: context.read<HomeNavigationCubit>().controller,
+          physics: const NeverScrollableScrollPhysics(),
+          children: const [
+            IngredientsScreen(),
+            RecipesScreen()
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            switch (context.read<HomeNavigationCubit>().state) {
+              case HomeNavigationState.ingredients: showAddIngredientDialog(context); return;
+              case HomeNavigationState.recipes: showAddRecipeDialog(context); return;
+            }
+          },
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: const HomeNavBar(),
       )
     );
   }
