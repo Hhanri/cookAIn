a4331728196affbb270460a5c98c68b11cbdd4d8
HUYNH Henry <henry018@laposte.net>
2023-08-15 16:37:42 +0200

    chat bot list view and chat bot textfield

---
+++ lib/chatbot/presentation/widgets/chat_bot_list_view_widget.dart
@@ -0,0 +1,25 @@
+import 'package:cookain/chatbot/domain/entities/chat_bot_message_entity.dart';
+import 'package:cookain/chatbot/presentation/cubits/chat_bot_cubit/chat_bot_cubit.dart';
+import 'package:cookain/core/widgets/query_list_view_builder.dart';
+import 'package:flutter/material.dart';
+
+class ChatBotListViewWidget extends StatelessWidget {
+  const ChatBotListViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return QueryListViewBuilder<ChatBotCubit, ChatBotMessageEntity>(
+      reverse: true,
+      emptyBuilder: (context) {
+        return const Center(
+          child: Text(
+            "Start you conversation with CookAIn's Chat Bot"
+          ),
+        );
+      },
+      itemBuilder: (context, snapshot) {
+        return Text(snapshot.data().prompt);
+      }
+    );
+  }
+}
---
+++ lib/chatbot/presentation/widgets/chat_bot_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:cookain/chatbot/presentation/cubits/chat_bot_cubit/chat_bot_cubit.dart';
+import 'package:cookain/core/widgets/text_field_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ChatBotTextField extends StatelessWidget {
+  const ChatBotTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<ChatBotCubit>();
+    return Row(
+      children: [
+        Expanded(
+          child: MyTextField(
+            params: NormalTextFieldParameters(label: 'your message...'),
+            controller: cubit.controller
+          ),
+        ),
+        IconButton(
+          onPressed: cubit.sendMessage,
+          icon: const Icon(Icons.send)
+        )
+      ],
+    );
+  }
+}
