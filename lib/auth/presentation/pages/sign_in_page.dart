778b588598e400bbd104ec6a25bce0b120032a1a
HUYNH Henry <henry018@laposte.net>
2023-08-17 18:22:27 +0200

    sign in page with asset and updated sign in button

--- lib/auth/presentation/pages/sign_in_page.dart
+++ lib/auth/presentation/pages/sign_in_page.dart
@@ -8,29 +8,36 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Placeholder(),
-          const Text("Welcome to CookAIn"),
-          BlocBuilder<AuthCubit, AuthState>(
-            builder: (context, state) {
-              if (state is AuthSignedIn) {
-                return const Text("Signed in");
-              }
-              if (state is AuthSignedOut) {
-                return ElevatedButton(
-                  onPressed: context.read<AuthCubit>().signInWithGoogle,
-                  child: const Text("Sign in with Google")
-                );
-              }
-              if (state.isLoading) {
-                return const CircularProgressIndicator();
-              }
-              return const SizedBox.shrink();
-            },
-          )
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2,),
+            FractionallySizedBox(
+              widthFactor: 0.6,
+              child: Image.asset(
+                'assets/livre-de-recettes.png'
+              ),
+            ),
+            const Spacer(flex: 2,),
+            Text(
+              "Welcome to CookAIn",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const Spacer(),
+            Text(
+              "Keep track of your food supply and your cookbook !",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const Spacer(flex: 2,),
+            FilledButton.tonalIcon(
+              onPressed: context.read<AuthCubit>().signInWithGoogle,
+              icon: const Text("Sign in with Google"),
+              label: const Icon(Icons.arrow_forward),
+            ),
+            const Spacer(flex: 2,),
+          ],
+        ),
       ),
     );
   }
